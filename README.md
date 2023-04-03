c4b7f07cc4afce67d503df95ef844c40dac0997a
Raiya Young <121911949+bundleofcodes@users.noreply.github.com>
2023-04-03 11:05:27 -0400

    Edits to code for shapes.js. edits to shapes.test.js and ran to make sure all tests were passed. Edits to index.js to ensure the code was writing to the correct file.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# my-svg-logo-generator
+# My-SVG-Logo-Generator
+
+## Description:
+
+The purpose of this project was to create an application that enables users to enter their inputs into an inquirer prompt in order to generate a logo.svg file, which contains the users desired results and rendering an SVG image. I learned how to run the debug with help of Jest to test the code for the shapes to ensure all parameters passed testing to allow for the apllication to function properly. 
+
+## Installation:
+
+- Inquirer.js: version 8.2.4
+- Jest: version  29.5.0
+- Node.JS
+
+## Usage:
+
+- [Application Video Walkthrough](https://drive.google.com/file/d/15mVwPPAaJ8cFJ8AsDHohNnAYh3rLNk7a/view?usp=sharing) 
+
+- Application Screenshot:
+
+- ![Application Screenshot](./README%20preview%20Screenshot.png)
+
+- [Repository URL](https://github.com/bundleofcodes/pro-README.md-generator)
+
+
--- index.js
+++ index.js
@@ -7,24 +7,13 @@ class Svg {
         this.texElement = ''
         this.shapeElement = ''
     }
-    render(){
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg%22%3E">
-
-    <circle cx="150" cy="100" r="80" fill="green"/>
-  
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
-  
-  </svg>${this.shapeElement}${this.texElement}</svg>`
+    render(text, textColor, shape) {
+            return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            ${shape.render()}
+            <text x="90" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+            </svg>`
     }
-    setTextElement(text,color) {
-        this.setTextElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
-    setShapeElement(shape) {
-        this.shapeElement = shape.render()
-    }
-
-}
-
+};
 //Array of questions for user input//
 const questions = [
     {
@@ -48,27 +37,22 @@ const questions = [
         name: "shape",
         message: "SHAPE-COLOR: Enter a color keyword (OR a hexidecimal number):",
     },
-    
 ];
-
 //Function to write files//
 function writeToFile(data) {
     console.log("writing [" + data + ") to file Output")
     fs.writeFile("./output/logo.svg", data, function (err) {
         if (err) {
             return console.log(err);
-        } 
+        }
         console.log("Your logo.svg has been successfully generated!");
     });
 }
-
 async function init() {
     console.log("starting init");
     var svgString = "";
-
 //Prompt the user for answers//
 const answers = await inquirer.prompt(questions);
-
 //user text//
 var user_text = "";
 if (answers.text.length > 0 && answers.text.length < 4) {
@@ -89,38 +73,28 @@ console.log("User shape color: [" + user_shape_color + "]");
 //user shape type//
 user_shape_type = answers["pixel-image"];
 console.log("User entered shape = [" + user_shape_type +"]");
-
 //user shape//
 let user_shape;
-    if (user_shape_type === "Square" || user_shape_type ==="square") {
-        user_shape = new Square();
-        console.log("User selected Square shape");
-    }
-    else if (user_shape_type === "Circle" || user_shape_type === "circle") {
-            user_shape = new Circle();
-            console.log("User selceted Circle shape");
-    }
-    else if (user_shape_type === "Triangle" || user_shape_type === "triangle") {
-            user_shape = new Triangle();
-            console.log("User selected Triangle shape");
-    }
-    else {
-        console.log("Oops, Invalid input!");
-    }
-    user_shape.setColor(user_shape_color);
-
+switch (user_shape_type) {
+    case 'Circle':
+    user_shape = new Circle(user_shape_color);
+    break;
+  case 'Square':
+    user_shape = new Square(user_shape_color);
+    break;
+  case 'Triangle':
+    user_shape = new Triangle(user_shape_color);
+    break;
+  default:
+    console.log('Invalid input!');
+}
 //Create new svg and add shape and text elements//
 var svg = new Svg();
-    svg.setTextElement(user_text, user_font_color);
-    svg.setShapeElement(user_shape);
-    svgString = svg.render();
-    
+    svgString = svg.render(user_text, user_font_color, user_shape)
 //Print shape to console//
     console.log("Dispalying shape:\n\n" + svgString);
-    
     console.log("Your shape has been generated!");
     console.log("Writing shape to file...");
     writeToFile(svgString);
-
 }
 init()
